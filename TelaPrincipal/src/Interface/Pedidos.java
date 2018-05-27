88277f1c80993a1f8043f11af223685f3c2a339a
renatomeucorreio <renatomeucorreio@gmail.com>
2018-05-26 23:44:46 -0300

    Tela Principal mais mini tela - Esboço

---
+++ TelaPrincipal/src/Interface/Pedidos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Renato
+ */
+public class Pedidos {
+    private String nome;
+    private String campanha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCampanha() {
+        return campanha;
+    }
+
+    public void setCampanha(String campanha) {
+        this.campanha = campanha;
+    }
+    
+    
+}
